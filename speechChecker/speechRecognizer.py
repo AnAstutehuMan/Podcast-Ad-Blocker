de85494b79202a7c5c27b918715c78ec57f44657
Saketh <sakethmadhu7@gmail.com>
2020-05-27 16:31:43 -0700

    Update speechRecognizer.py

--- speechChecker/speechRecognizer.py
+++ speechChecker/speechRecognizer.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-clip = sr.AudioFile('clip.wav')
+clip = sr.AudioFile('speechChecker\clip.wav')
 
 with clip as source:
     audio = r.record(source)
