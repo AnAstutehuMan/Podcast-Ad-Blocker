0678c5f19207d51376c5d4dc008599f273063db4
Saketh <sakethmadhu7@gmail.com>
2020-05-28 11:51:05 -0700

    dabble with code

--- speechChecker/speechRecognizer.py
+++ speechChecker/speechRecognizer.py
@@ -3,15 +3,19 @@ import pydub as pd
 import math
 from playsound import playsound
 from os import path
+import wave
+import soundfile as sf
+import librosa
 
 r = sr.Recognizer()
 
-audiopath = path.join(path.dirname(__file__),"files/test.wav")
+audiopath = path.join(path.dirname(__file__), "files/test.wav")
 
 audioclip = []
 
 loadedaudio = []
 reconaudio = []
+
 
 def recognize_clips(step, repeat):
     global audioclip, loadedaudio, reconaudio
@@ -22,20 +26,20 @@ def recognize_clips(step, repeat):
             reconaudio[i] = r.recognize_google(loadedaudio[i])
             print(str(i)+"   "+reconaudio[i])
 
+
 def populate_times(path, time):
     global audioclip, loadedaudio, reconaudio
     audio = pd.AudioSegment.from_file(audiopath, "wav")
     step = time * 1000
     repeat = audio.duration_seconds % step
-    carry =  repeat - math.floor(repeat)
+    carry = repeat - math.floor(repeat)
     repeat = math.floor(repeat)
     for i in range(repeat):
         export = audio[i * step: (i+1) * step].export("clip"+str(i)+".wav")
         audioclip.append(export)
-    #recognize_clips(step, repeat)
+    recognize_clips(step, repeat)
 
 
 populate_times(audiopath, 5)
 
-#playsound(audiopath)
-
+# playsound(audiopath)
