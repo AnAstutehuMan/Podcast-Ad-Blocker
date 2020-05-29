c158f1cc39250940828ca57aff0be7e6011147c4
Saketh <sakethmadhu7@gmail.com>
2020-05-29 14:54:21 -0700

    rewritten from ground

--- speechChecker/speechRecognizer.py
+++ speechChecker/speechRecognizer.py
@@ -1,11 +1,12 @@
-import speech_recognition as sr
-import pydub as pd
 import math
-from playsound import playsound
-from os import path
 import wave
+from os import path
+
+import pydub as pd
+import speech_recognition as sr
+from pydub import AudioSegment
+
 import soundfile as sf
-import librosa
 
 r = sr.Recognizer()
 
@@ -23,17 +24,20 @@ def recognize_clips(step, repeat):
         with sr.AudioFile(audioclip[i]) as source:
             loadedaudio.append(r.record(source))
             reconaudio.append(r.recognize_google(loadedaudio[i]))
-            print("Time : "+str(i*step/1000)+" - "+str((i+1)*step/1000)+"| Words : "+str(reconaudio[i]))
+            print("Time : "+str(i*step/1000)+" - " +
+                  str((i+1)*step/1000)+"| Words : "+str(reconaudio[i]))
+
 
 def populate_times(path, time):
     global audioclip, loadedaudio, reconaudio
     audio = pd.AudioSegment.from_file(audiopath, "wav")
     step = time * 1000
     repeat = audio.duration_seconds / time
-    carry =  repeat - math.floor(repeat)
+    carry = repeat - math.floor(repeat)
     repeat = math.floor(repeat)
     for i in range(repeat):
-        export = audio[i * step: (i+1) * step].export("clip"+str(i)+".wav", format = "wav")
+        export = audio[i * step: (i+1) * step].export("clip" +
+                                  str(i)+".wav", format="wav")
         audioclip.append("clip"+str(i)+".wav")
     recognize_clips(step, repeat)
 
@@ -41,3 +45,36 @@ def populate_times(path, time):
 populate_times(audiopath, 1.5)
 
 # playsound(audiopath)
+
+
+# Saketh: rewrote the code
+
+
+r = sr.Recognizer()
+f = open("transcript", "a")
+
+
+def splitAudio(filePath, interval):
+    intervalOne = 0
+    intervalTwo = interval * 1000
+
+    numberOfSplits = math.floor(AudioSegment.from_wav(
+        filePath).duration_seconds / interval)
+
+    for i in range(numberOfSplits):
+        AudioSegment.from_wav(filePath)[intervalOne:intervalTwo].export(
+            "clip" + str(i) + ".wav", format="wav")
+        intervalOne = intervalTwo
+        intervalTwo = intervalTwo + (interval * 1000)
+
+    recognizeAudio(filePath, numberOfSplits)
+
+
+def recognizeAudio(filePath, numberOfSplits):
+    for i in range(numberOfSplits):
+        with sr.AudioFile("clip" + str(i) + ".wav") as source:
+            f.write(r.recognize_google(r.record(source))))
+    f.close()
+
+
+splitAudio('clip.wav', 2)
