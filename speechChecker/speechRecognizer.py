48127b1a6ae72235765c31dfdace79710998c3f6
lynnlo <tharlynnhtet.ong@gmail.com>
2020-05-28 19:03:22 -0700

    Added Timestamp Support Beta

--- speechChecker/speechRecognizer.py
+++ speechChecker/speechRecognizer.py
@@ -16,26 +16,25 @@ reconaudio = []
 def recognize_clips(step, repeat):
     global audioclip, loadedaudio, reconaudio
     for i in range(repeat):
-        print(audioclip[i])
         with sr.AudioFile(audioclip[i]) as source:
-            loadedaudio[i] = r.record(source)
-            reconaudio[i] = r.recognize_google(loadedaudio[i])
-            print(str(i)+"   "+reconaudio[i])
+            loadedaudio.append(r.record(source))
+            reconaudio.append(r.recognize_google(loadedaudio[i]))
+            print("Time : "+str(i*step/1000)+" - "+str((i+1)*step/1000)+"| Words : "+str(reconaudio[i]))
 
 def populate_times(path, time):
     global audioclip, loadedaudio, reconaudio
     audio = pd.AudioSegment.from_file(audiopath, "wav")
     step = time * 1000
-    repeat = audio.duration_seconds % step
+    repeat = audio.duration_seconds / time
     carry =  repeat - math.floor(repeat)
     repeat = math.floor(repeat)
     for i in range(repeat):
-        export = audio[i * step: (i+1) * step].export("clip"+str(i)+".wav")
-        audioclip.append(export)
-    #recognize_clips(step, repeat)
+        export = audio[i * step: (i+1) * step].export("clip"+str(i)+".wav", format = "wav")
+        audioclip.append("clip"+str(i)+".wav")
+    recognize_clips(step, repeat)
 
 
-populate_times(audiopath, 5)
+populate_times(audiopath, 1.5)
 
 #playsound(audiopath)
 
