df88044090bd116c8d6f92a48be30fce3b4d27c3
Saketh <sakethmadhu7@gmail.com>
2020-06-05 16:01:37 -0700

    Update speechRecognizer.py

--- speechChecker/speechRecognizer.py
+++ speechChecker/speechRecognizer.py
@@ -6,6 +6,7 @@ from pydub import AudioSegment
 
 filterwords = ["sponser"]
 
+
 def splitAudio(filePath, interval):
     # Mutiformat support
     audio = AudioSegment.from_file(filePath, "wav")
@@ -26,18 +27,22 @@ def recognizeAudio(filePath, numberOfSteps):
     f.write("{")
     for i in range(numberOfSteps):
         with speech_recognition.AudioFile("clip (" + str(i) + ").wav") as source:
-            f.write('"' + str(i) + '":"' + r.recognize_google(r.record(source)) + '"')
+            f.write('"' + str(i) + '":"' +
+                    r.recognize_google(r.record(source)) + '"')
         if i != numberOfSteps - 1:
             f.write(','+"\n")
     f.write("}")
     f.close()
+    filterAudio()
 
 
 def filterAudio():
     f = open("transcript.json", "r+")
     for line in f:
-        if (line.split() == filterwords):
-            pass
+        for i in range:
+            if (line.split() == filterwords[i]):
+                line.strip("\n")
+                f.write(line)
 
 
 splitAudio('clip.wav', 1.5)
