ca2638b08683cbdc35ffea31e5b1d200bed0f598
Saketh <sakethmadhu7@gmail.com>
2020-06-05 12:35:34 -0700

    clutch time!

--- speechChecker/speechRecognizer.py
+++ speechChecker/speechRecognizer.py
@@ -1,5 +1,6 @@
-import math
 import json
+import math
+
 import speech_recognition
 from pydub import AudioSegment
 
@@ -14,7 +15,7 @@ def splitAudio(filePath, interval):
     # More simple spliting
     for i in range(numberOfSteps):
         AudioSegment.from_wav(filePath)[
-            i * amountToStep:(i+1) * amountToStep].export("clip" + str(i) + ".wav", format="wav")
+            i * amountToStep:(i + 1) * amountToStep].export("clip (" + str(i) + ").wav", format="wav")
 
     recognizeAudio(filePath, numberOfSteps)
 
@@ -24,13 +25,19 @@ def recognizeAudio(filePath, numberOfSteps):
     f = open("transcript.json", "w+")
     f.write("{")
     for i in range(numberOfSteps):
-        with speech_recognition.AudioFile("clip" + str(i) + ".wav") as source:
-            f.write('"'+str(i)+'":"'+r.recognize_google(r.record(source))+'"')
+        with speech_recognition.AudioFile("clip (" + str(i) + ").wav") as source:
+            f.write('"' + str(i) + '":"' + r.recognize_google(r.record(source)) + '"')
         if i != numberOfSteps - 1:
             f.write(','+"\n")
     f.write("}")
     f.close()
 
+
 def filterAudio():
-    f = open("transcript.json","r+")
-splitAudio('test.wav', 1.5)
+    f = open("transcript.json", "r+")
+    for line in f:
+        if (line.split() == filterwords):
+            pass
+
+
+splitAudio('clip.wav', 1.5)
